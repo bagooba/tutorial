3e99e791f5ee9074e16d3c59a43548a9807578d7
M Harris <mallory@Ms-MacBook-Air.local>
2016-05-03 14:43:37 -0400

    Chapter 6 7 8

--- src/clj/tutorial/core.clj
+++ src/clj/tutorial/core.clj
@@ -212,7 +212,7 @@
         :attributes {:intelligence 10 
                      :strength 4 
                      :dexterity 5}})
-(def c-int (comp :intelligence :attributes))
+;(def c-int (comp :intelligence :attributes))
 (def c-str (comp :strength :attributes))
 (def c-dex (comp :dexterity :attributes))
 
@@ -231,3 +231,122 @@
 (defn my-update-in 
   ([m [k & ks] f]
    (assoc-in m (into [k] ks) (f (reduce get (get m k) ks)))))
+
+;chapter 7 code
+
+(defn infix
+    [infixed]
+    (list (second infixed) 
+          (first infixed) 
+          (last infixed)))
+
+(defn multi-operator [coll]
+  (loop [lst coll 
+         idx 0]
+        (if (or (= (nth lst idx) '*) (= (nth lst idx) '/))
+          idx
+          (recur lst 
+                 (+ idx 1)))))
+
+(defn add-operator [coll]
+  (loop [lst coll 
+         idx 0]
+        (if (or (= (nth lst idx) '+) (= (nth lst idx) '-))
+          idx
+          (recur lst 
+                 (+ idx 1)))))
+
+(defn making-multi [coll]
+  (let [idx (multi-operator coll)
+        infixed (infix (take 3 (drop (- idx 1) coll)))]
+    (concat (take (dec idx) coll) (list infixed) (drop (+ idx 2) coll))))
+
+(defn making-add [coll]
+  (let [idx (add-operator coll)
+        infixed (infix (take 3 (drop (- idx 1) coll)))]
+    (concat (take (dec idx) coll) (list infixed) (drop (+ idx 2) coll))))
+
+(defn making-eqn [coll]
+  (loop [eqn coll]
+        (cond 
+          (= (list (first eqn)) eqn)
+          (first eqn)
+          (or (.contains eqn '*) (.contains eqn '/))
+          (recur (making-multi eqn))
+          (or (.contains eqn '+) (.contains eqn '-))
+          (recur (making-add eqn)))))
+
+
+;Chapter 8 problems
+
+;book code
+(def order-details
+  {:name "Mitchard Blimmons"
+   :email "mitchard.blimmonsgmail.com"})
+
+(def order-details-validations
+  {:name
+   ["Please enter a name" not-empty]
+   :email 
+   ["Please enter an email address" not-empty 
+    "Your email address doesn't look like an email address"
+    #(or (empty? %) (re-seq #"@" %))]})
+
+(defn error-messages-for
+  "Return a seq of error messages"
+  [to-validate message-validator-pairs]
+  (map first (filter #(not ((second %) to-validate))
+                     (partition 2 message-validator-pairs))))
+
+(defn validate
+  "Returns a map with a vector of errors for each key"
+  [to-validate validations]
+  (reduce (fn [errors validation]
+              (let [[fieldname validation-check-groups] validation
+                    value (get to-validate fieldname)
+                    error-messages (error-messages-for value validation-check-groups)]
+                (if (empty? error-messages)
+                  errors
+                  (assoc errors fieldname error-messages))))
+          {}
+          validations))
+
+(defn if-valid
+  [record validations success-code failure-code]
+  (let [errors (validate record validations)]
+    (if (empty? errors)
+      success-code
+      failure-code)))
+
+(defmacro if-valid
+  "Handle validation more concisely"
+  [to-validate validations errors-name & then-else]
+  `(let [~errors-name (validate ~to-validate ~validations)]
+     (if (empty? ~errors-name)
+       ~@then-else)))
+;1
+
+(defmacro when-valid 
+  [to-validate validations & stuff]
+  `(if (empty? (validate ~to-validate ~validations))
+     (do ~@stuff)))
+
+;2
+
+(defmacro my-or 
+  ([] false)
+  ([x] x)
+  ([x & next]
+   `(let [or# ~x]
+      (if or# 
+        or# 
+        (my-or ~@next)))))
+
+;3
+
+(defmacro defattrs
+  ([retriever attr] 
+   `(def ~retriever (comp ~attr :attributes)))
+  ([retriever attr & other]
+   `(do (defattrs ~retriever ~attr) (defattrs ~@other))))
+   
